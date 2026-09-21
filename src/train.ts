0679fcd0203d4eb6267a7f661e7cf4f547dea4c6
Bilolbek <mamadrayimovbilolbek@gmail.com>
2026-09-21 18:55:28 +0900

    feat: M Task

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,19 @@
-console.log("Hello world!")
+//  M Task 
+
+function getSquareNumbers(numbersList) {
+    let result = [];
+
+    for (let i = 0; i < numbersList.length; i++) {
+        let currentNumber = numbersList[i];
+        let squaredValue = currentNumber * currentNumber;
+
+        result.push({
+            number: currentNumber,
+            square: squaredValue,
+        });
+    }
+    return result;
+}
+
+console.log(getSquareNumbers([5, 10, 20, 90, 700]));
+
