572f9a07e8d4612947fba58463960442f38dcafe
Bilolbek <mamadrayimovbilolbek@gmail.com>
2026-09-25 18:45:19 +0900

    feat: create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import router from "./routers"
 
 /** 1-Entrance **/
 const app = express();
@@ -15,5 +16,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs")
 
 /** 4-Routers **/
+app.use("/", router); // Middleware Design Pattern
+
 
 export default app;    // module.export = app;
--- src/train.ts
+++ src/train.ts
@@ -1,19 +1,19 @@
-//  M Task 
+// //  M Task 
 
-function getSquareNumbers(numbersList) {
-    let result = [];
+// function getSquareNumbers(numbersList) {
+//     let result = [];
 
-    for (let i = 0; i < numbersList.length; i++) {
-        let currentNumber = numbersList[i];
-        let squaredValue = currentNumber * currentNumber;
+//     for (let i = 0; i < numbersList.length; i++) {
+//         let currentNumber = numbersList[i];
+//         let squaredValue = currentNumber * currentNumber;
 
-        result.push({
-            number: currentNumber,
-            square: squaredValue,
-        });
-    }
-    return result;
-}
+//         result.push({
+//             number: currentNumber,
+//             square: squaredValue,
+//         });
+//     }
+//     return result;
+// }
 
-console.log(getSquareNumbers([5, 10, 20, 90, 700]));
+// console.log(getSquareNumbers([5, 10, 20, 90, 700]));
 
